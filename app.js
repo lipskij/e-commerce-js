62a3d05bd7c6d6a6dfa642716507fc1099914dae
lipskij <emil.lipskij@gmail.com>
2020-07-13 14:04:14 +0300

    add contentful

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "l2ke6s697lzd",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "kA4CGHy_yedOXuOiHoQIKP1qS6A16b-pVbc9cBreuDk"
+});
+
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
@@ -17,10 +24,14 @@ let buttonsDOM = [];
 class Products {
   async getProducts() {
     try {
-      let result = await fetch('products.json');
-      let data = await result.json();
+      let contentful = await client.getEntries({
+        content_type: "bikeShopProducts"
+      });
 
-      let products = data.items;
+      // let result = await fetch('products.json');
+      // let data = await result.json();
+
+      let products = contentful.items;
       products = products.map(item => {
         const {title, price} = item.fields;
         const {id} = item.sys;
